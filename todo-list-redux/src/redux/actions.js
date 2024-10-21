e86c1de0991712bb2839addf7923f826cff48005
SiddeshGD <siddeshgd@skulogic.com>
2024-10-21 14:51:34 +0530

    Update actions.js

--- todo-list-redux/src/redux/actions.js
+++ todo-list-redux/src/redux/actions.js
@@ -1,9 +1,10 @@
 let nextId = 1;
 
+//Add new Todo list
 export const addTodo = (text, expectedEndDate, description) => ({
   type: 'ADD_TODO',
   payload: {
-    id: nextId++, // Increment the ID for each new todo
+    id: nextId++, // Increment the ID for each new todo 
     text,
     status: 'pending',
     createdAt: new Date().toISOString(),
@@ -11,16 +12,18 @@ export const addTodo = (text, expectedEndDate, description) => ({
     description,
   },
 });
-
+//Modify the to-do-list
 export const modifyTodo = (id, text, expectedEndDate, description) => ({
   type: 'MODIFY_TODO',
   payload: { id, text, expectedEndDate, description },
 });
 
+//Delete the todo-list
 export const deleteTodo = id => ({
   type: 'DELETE_TODO',
   payload: id,
 });
+
 
 export const setStatus = (id, status) => ({
   type: 'SET_STATUS',
